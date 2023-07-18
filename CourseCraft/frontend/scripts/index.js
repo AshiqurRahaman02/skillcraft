33bc75e6bc83ed44739b62828fc5a1c9e6213aa9
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-07-19 00:08:17 +0530

    fw24-616-day1

---
+++ CourseCraft/frontend/scripts/index.js
@@ -0,0 +1,62 @@
+const userDetails = JSON.parse(localStorage.getItem('userDetails')) || null;
+
+const redirect = () => {
+    if(userDetails){
+        window.location.href = "/pages/account.html"
+    }else{
+        window.location.href = "/pages/sign.html"
+    }
+}
+
+
+const bottomNavDiv = document.querySelector("#bottomNav>div");
+
+const shearchBtn = document.querySelector("#topNav>div:nth-child(3)>button");
+shearchBtn.addEventListener("click", () => {
+	bottomNavDiv.innerHTML = `
+                                <ion-icon name="search-outline"></ion-icon>
+                                <input type="search" placeholder="What do you want to learn?" />
+                                
+                            `;
+	document.querySelector("#bottomNav").style.display = "block";
+});
+
+const hideBottomNav = () => {
+	document.querySelector("#bottomNav").style.display = "none";
+};
+
+const showSkills = () => {
+    bottomNavDiv.innerHTML = `
+                                <a href=""><img src="./images/javascript.svg" alt=""><span>JAVASCRIPT</span></a>
+                                <a href=""><img src="./images/nodejs.svg" alt=""><span>NODE.JS</span></a>
+                                <a href=""><img src="./images/python-5.svg" alt=""><span>PYTHON</span></a>
+                                <a href=""><img src="./images/react-1.svg" alt=""><span>REACT</span></a>
+                                <a href=""><img src="./images/aws-2.svg" alt="" id="aws"><span>AWS</span></a>
+                                <a href=""><img src="./images/c.svg" alt=""><span>C++</span></a>
+                                <a href=""><img src="./images/html-1.svg" alt="" id="aws"><span>HTML</span></a>
+                                <a href=""><img src="./images/css-3.svg" alt=""><span>CSS</span></a>
+                                <a href=""><img src="./images/tailwind-css-2.svg" alt="" id="aws"><span> Tailwind CSS</span></a>
+                                <a href=""><span>SEE MORE</span><ion-icon name="arrow-forward-outline"></ion-icon></a>
+                            `;
+	document.querySelector("#bottomNav").style.display = "block";
+}
+const showCourses = () => {
+    bottomNavDiv.innerHTML = `
+                                <a href=""><img src="./images/web.svg" alt=""><span>WEB DEVELOPMENT</span></a>
+                                <a href=""><img src="./images/photoshop.svg" alt=""><span>PHOTOSHOP CC</span></a>
+                                <a href=""><img src="./images/ai.svg" alt=""><span>AI DEVELOPMENT</span></a>
+                                <a href=""><img src="./images/tailwind-css-2.svg" alt="" id="aws"><span> Tailwind CSS</span></a>
+                                <a href=""><span>SEE MORE</span><ion-icon name="arrow-forward-outline"></ion-icon></a>
+                            `;
+	document.querySelector("#bottomNav").style.display = "block";
+}
+const showPlans = () => {
+    bottomNavDiv.innerHTML = `
+                                <h1>
+                                    <span>BASIC PLAN : $100/ MONTH</span>
+                                    <span>STANDARD PLAN : $1000/ YEAR</span>
+                                    <span>1 MONTH FREE TRAIL</span>
+                                </h1>
+                            `;
+	document.querySelector("#bottomNav").style.display = "block";
+}
