ff735db22d5b605c44edd3b2911bb8d0bf15ecad
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-07-23 09:01:36 +0530

    fixing bugs

--- CourseCraft/frontend/scripts/account.js
+++ CourseCraft/frontend/scripts/account.js
@@ -73,7 +73,7 @@ const showPlans = () => {
 
 
 if (!userDetails) {
-  window.location.href = "../pages/signin.html";
+	window.location.href = "../pages/signin.html";
 }
 const userID = userDetails._id;
 const email = userDetails.email;
@@ -85,66 +85,53 @@ var activeSection = "";
 
 const params = new URLSearchParams(window.location.search);
 
-const activeParam = params.get("active");
+const activeParam = params.get("active"); // "video"
 
 if (activeParam === "video") {
-  displayVideos();
-  console.log("Video is active.");
+	displayVideos();
+	console.log("Video is active.");
 } else if (activeParam === "some_other_value") {
-  console.log("Some other value is active.");
+	console.log("Some other value is active.");
 } else {
-  console.log("Default state.");
-  displayAccount();
+	console.log("Default state.");
+	displayAccount()
+	
 }
 function displayAccount() {
-  if (activeSection !== "account") {
-    document.getElementById("account").style.display = "block";
-    activeSection = "account";
+	if (activeSection !== "account") {
+		document.getElementById("account").style.display = "block";
+		activeSection = "account";
 
-    document.getElementById("videos").style.display = "none";
-    document.getElementById("subscribe").style.display = "none";
-    document.getElementById("plans").style.display = "none";
-    document.getElementById("notifications").style.display = "none";
-  }
+		document.getElementById("videos").style.display = "none";
+		document.getElementById("subscribe").style.display = "none";
+		document.getElementById("plans").style.display = "none";
+		document.getElementById("notifications").style.display = "none";
+
+	}
 }
 
 function displayVideos() {
-  if (activeSection !== "videos") {
-    document.getElementById("videos").style.display = "block";
-    activeSection = "videos";
+	if (activeSection !== "videos") {
+		document.getElementById("videos").style.display = "block";
+		activeSection = "videos";
+		
+		document.getElementById("account").style.display = "none";
+		document.getElementById("subscribe").style.display = "none";
+		document.getElementById("plans").style.display = "none";
+		document.getElementById("notifications").style.display = "none";
 
-    document.getElementById("account").style.display = "none";
-    document.getElementById("subscribe").style.display = "none";
-    document.getElementById("plans").style.display = "none";
-    document.getElementById("notifications").style.display = "none";
-
-    // get user video
-    fetch(`http://localhost:8080/video/get/videos/${userID}`)
-      .then((response) => response.json())
-      .then((data) => {
-        displayAllVideos(data.videos);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
+		// get user video
+		fetch(`http://localhost:8080/video/get/videos/${userID}`)
+			.then((response) => response.json())
+			.then((data) => {
+				displayAllVideos(data.videos);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	}
 }
 function displayAllVideos(data) {
-
-  console.log(data);
-  data.reverse();
-  let html = "";
-
-  data.forEach((video) => {
-    let title = video.name;
-    let maxLength = 14;
-    if (title.length > maxLength) {
-      title = title.substring(0, maxLength) + "...";
-    }
-    html += `
-              <div class="video_box">
-                <div class="video_thumbnail">
-
 	console.log(data);
 	data.reverse()
 	let html = "";
@@ -158,7 +145,6 @@ function displayAllVideos(data) {
 		html += `
               <div class="video_box" >
                 <div class="video_thumbnail"  onclick="openVideo('${video._id}')">
-
                   <img src="${video.thumbnailURL}" alt="" />
                 </div>
                 <div class="video_nameAndbtn">
@@ -179,34 +165,10 @@ function displayAllVideos(data) {
                 </div>
               </div>
             `;
-  });
+	});
 
-  document.getElementById("parent").innerHTML = html;
+	document.getElementById("parent").innerHTML = html;
 }
-
-async function deleteVideo(title, id) {
-  if (confirm(`Are you sure you want to delete ${title}`)) {
-    try {
-      const response = await fetch(`http://localhost:8080/video/delete/${id}`, {
-        method: "DELETE",
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        console.log(data.message);
-        window.location.reload();
-      } else {
-        console.error(data.message);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  } else {
-    return;
-  }
-}
-
 function openVideo(id) {
 	let url = "../pages/video.html?id=" + id;
 	window.location.href = url;
@@ -241,107 +203,76 @@ async function deleteVideo(title,id) {
 	window.location.href = "../pages/signin.html"
   }
 
-
 function displaySubscribers() {
-  if (activeSection !== "subscribe") {
-    document.getElementById("subscribe").style.display = "block";
-    activeSection = "subscribe";
-
-    document.getElementById("account").style.display = "none";
-    document.getElementById("videos").style.display = "none";
-    document.getElementById("plans").style.display = "none";
-    document.getElementById("notifications").style.display = "none";
-  }
+	if (activeSection !== "subscribe") {
+		document.getElementById("subscribe").style.display = "block";
+		activeSection = "subscribe";
+		
+		document.getElementById("account").style.display = "none";
+		document.getElementById("videos").style.display = "none";
+		document.getElementById("plans").style.display = "none";
+		document.getElementById("notifications").style.display = "none";
+	}
 }
 
 function displayPlans() {
-  if (activeSection !== "plans") {
-    document.getElementById("plans").style.display = "block";
-    activeSection = "plans";
-
-    document.getElementById("account").style.display = "none";
-    document.getElementById("subscribe").style.display = "none";
-    document.getElementById("videos").style.display = "none";
-    document.getElementById("notifications").style.display = "none";
-  }
+	if (activeSection !== "plans") {
+		document.getElementById("plans").style.display = "block";
+		activeSection = "plans";
+		
+		document.getElementById("account").style.display = "none";
+		document.getElementById("subscribe").style.display = "none";
+		document.getElementById("videos").style.display = "none";
+		document.getElementById("notifications").style.display = "none";
+	}
 }
 
 function displayNotifications() {
-  if (activeSection !== "notifications") {
-    document.getElementById("notifications").style.display = "block";
-    activeSection = "notifications";
-
-    document.getElementById("account").style.display = "none";
-    document.getElementById("subscribe").style.display = "none";
-    document.getElementById("plans").style.display = "none";
-    document.getElementById("videos").style.display = "none";
-  }
+	if (activeSection !== "notifications") {
+		document.getElementById("notifications").style.display = "block";
+		activeSection = "notifications";
+		
+		document.getElementById("account").style.display = "none";
+		document.getElementById("subscribe").style.display = "none";
+		document.getElementById("plans").style.display = "none";
+		document.getElementById("videos").style.display = "none";
+	}
 }
+
 
 var image = null;
 var video = null;
 
 function handleImageChange(event) {
-  const file = event.target.files?.[0];
-  if (file) {
-    console.log(file);
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      image = reader.result;
-      document.querySelector("#img").src = `${image}`;
-    };
-    reader.readAsDataURL(file);
-  } else {
-    console.log("Couldn't find file " + file);
-  }
+	const file = event.target.files?.[0];
+	if (file) {
+		console.log(file);
+		const reader = new FileReader();
+		reader.onloadend = () => {
+			image = reader.result;
+			document.querySelector("#img").src = `${image}`;
+		};
+		reader.readAsDataURL(file);
+	} else {
+		console.log("Couldn't find file " + file);
+	}
 }
 
 function handleVideoChange(event) {
-  const file = event.target.files?.[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      video = reader.result;
-      document.querySelector("video").src = `${reader.result}`;
-    };
-    reader.readAsDataURL(file);
-  } else {
-    console.log("Couldn't find file " + file);
-  }
+	const file = event.target.files?.[0];
+	if (file) {
+		const reader = new FileReader();
+		reader.onloadend = () => {
+			video = reader.result;
+			document.querySelector("video").src = `${reader.result}`;
+		};
+		reader.readAsDataURL(file);
+	} else {
+		console.log("Couldn't find file " + file);
+	}
 }
 
 async function uploadVideo() {
-
-  event.preventDefault();
-
-  const imageInput = document.getElementById("image");
-  const image = imageInput.files[0];
-
-  const videoInput = document.getElementById("video");
-  const video = videoInput.files[0];
-
-  const name = document.getElementById("name").value;
-  const description = document.getElementById("description").value;
-  const category = document.getElementById("category").value;
-
-  if (image && video && name && description && category) {
-    const formData = new FormData();
-    formData.append("image", image);
-    formData.append("video", video);
-    formData.append("name", name);
-    formData.append("description", description);
-    formData.append("category", category);
-    formData.append("adminID", userID);
-    formData.append("creatorName", name);
-    formData.append("email", email);
-
-    document.getElementById("popup").style.filter = "blur(20px)";
-    document.querySelector(".pro").style.display = "block";
-
-    setTimeout(() => {}, 5000);
-    setTimeout(() => {
-      confirm(`Video uploaded successfully.
-
 	event.preventDefault();
 
 	const imageInput = document.getElementById("image");
@@ -371,58 +302,34 @@ async function uploadVideo() {
 		setTimeout(() => {}, 5000);
 		setTimeout(() => {
 			confirm(`Video uploaded successfully.
-
 		You will receive an email notification when your video goes live. It usually takes less than a minute.`);
-      uploadFinalVideo(formData);
-    }, 10000);
-  } else {
-    alert("Please enter valid information");
-  }
+			uploadFinalVideo(formData);
+		}, 10000);
+	} else {
+		alert("Please enter valid information");
+	}
 }
 
 async function uploadFinalVideo(formData) {
-  try {
-    const response = await fetch("http://localhost:8080/video/upload/video", {
-      method: "POST",
-      body: formData,
-    });
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error("Error:", error);
-  }
+	try {
+		const response = await fetch("http://localhost:8080/video/upload/video", {
+			method: "POST",
+			body: formData,
+		});
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		console.error("Error:", error);
+	}
 }
 
 const uploadBtn = document.querySelector("#upload");
 uploadBtn.addEventListener("click", () => {
-  popup.classList.add("openpopup");
-  document.querySelector("main").style.filter = "blur(20px)";
+	popup.classList.add("openpopup");
+	document.querySelector("main").style.filter = "blur(20px)";
 });
 
 function closePopup() {
-  document.getElementById("popup").classList.remove("openpopup");
-  document.querySelector("main").style.filter = "blur(0px)";
+	document.getElementById("popup").classList.remove("openpopup");
+	document.querySelector("main").style.filter = "blur(0px)";
 }
-
-// ................................
-function showUserData() {
-  const userProfileContainer = document.getElementsByClassName("account");
-  const codeName = document.getElementById("usercodeName");
-  const userNameSpan = document.getElementById("userNameSpan");
-  const userEmailSpan = document.getElementById("userEmailSpan");
-
-  // Retrieve data from localStorage using the key (e.g., 'userData')
-  const userData = JSON.parse(localStorage.getItem("userInfo"));
-
-  if (userData) {
-    // Update the HTML with the fetched data
-    codeName.textContent = userData.name + "#123";
-    userNameSpan.textContent = userData.name;
-    userEmailSpan.textContent = userData.email;
-  } else {
-    alert("No user data found in LocalStorage.");
-  }
-}
-
-// Call the function to show user data when the page loads
-showUserData();
